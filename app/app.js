c72467b0b4b5bdedcd289671087d309b5b7df881
cmk <mohamedkhalil1997@hotmail.fr>
2021-01-15 12:41:55 +0100

    initial commit

---
+++ app/app.js
@@ -0,0 +1,27 @@
+var ingredients = [];
+const enableDatePicker = () => {
+    $('#picked-date').attr('disabled', false);
+}
+const disableDatePicker = () => {
+    $('#picked-date').attr('disabled', true);
+}
+const dateValue = (element) => {
+    $('#choosed').val(element.value);
+}
+const addIngredient = () => {
+    ingredients.push($('#ingredient').val());
+    $('#ingredients').html('');
+    ingredients.map((ingredient, index) => {
+        let item = `<div class="ingredient__item" id="ingredient-${index}">
+                        <button onclick="deleteIngredient('ingredient-${index}');" type="button" class="ingredient__item__delete"><i>-</i></button> 
+                        <span>${ingredient}</span>
+                    </div>`;
+        $('#ingredients').append(item);
+    })
+
+    $('#ingredient').val('');
+    console.log(ingredients);
+}
+const deleteIngredient = (ingredient) => {
+    $(`#${ingredient}`).remove();
+}
